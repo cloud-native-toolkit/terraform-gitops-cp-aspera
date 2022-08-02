7a4d55462a85fe65adfe9f83751238834f9ad197
Jyoti Rani <jyotirani10@in.ibm.com>
2022-08-02 12:52:49 +0530

    updated for ns
    Signed-off-by: Jyoti Rani <jyotirani10@in.ibm.com>

--- test/stages/stage1-aspera.tf
+++ test/stages/stage1-aspera.tf
@@ -3,7 +3,6 @@ module gitops-cp-aspera {
 
   gitops_config   = module.gitops.gitops_config
   git_credentials = module.gitops.git_credentials
-  namespace       = module.gitops_namespace.name
   server_name     = module.gitops.server_name
   catalog         = module.cp_catalogs.catalog_ibmoperators
   channel         = module.cp4i-dependencies.aspera.channel
